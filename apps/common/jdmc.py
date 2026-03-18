7d463bb85e298a7940b406aad1be17346eb75a6e
Bai <baijiangjie@gmail.com>
2026-03-18 18:56:39 +0800

    feat: add request_jdmc method

---
+++ apps/common/jdmc.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+import requests_unixsocket
+
+__all__ = ['request_jdmc']
+
+
+def request_jdmc(method='GET', path='', timeout=(5, 60)):
+    ''' path: JDMC API path, e.g. /jdmc/api/v1/apps/license '''
+    url = settings.JDMC_BASE_URL + path
+    with requests_unixsocket.Session() as session:
+        response = session.request(method, url, timeout=timeout)
+    return response
