079ba319242dc8149a0c69fd88245f734f12c5f6
akinarcak <akinarcak@gmail.com>
2026-08-02 16:23:55 +0300

    test: fully mock cloud provider SDK flows

--- apps/cloud_sync/tests.py
+++ apps/cloud_sync/tests.py
@@ -1,4 +1,6 @@
 from contextlib import nullcontext
+import sys
+from types import ModuleType
 from types import SimpleNamespace
 from unittest.mock import Mock, patch
 
@@ -10,6 +12,175 @@ from .services import queue_sync
 from .tasks import sync_cloud_execution
 from .sync import quarantine_instance
 from .validation import validate_custom_endpoint
+from .providers.aws import AWSProvider
+from .providers.azure import AzureProvider
+
+
+class CloudProviderMockTests(SimpleTestCase):
+    def test_aws_sdk_is_mocked_and_instances_are_normalized(self):
+        regional_client = Mock()
+        regional_client.get_paginator.return_value.paginate.return_value = [{
+            'Reservations': [{'Instances': [
+                {
+                    'InstanceId': 'i-linux',
+                    'State': {'Name': 'running'},
+                    'Tags': [{'Key': 'Name', 'Value': 'api-01'}],
+                    'PrivateIpAddress': '10.0.0.10',
+                    'PublicIpAddress': '203.0.113.10',
+                    'InstanceType': 't3.small',
+                },
+                {
+                    'InstanceId': 'i-windows',
+                    'State': {'Name': 'stopped'},
+                    'Platform': 'windows',
+                    'PrivateIpAddress': '10.0.0.11',
+                },
+                {
+                    'InstanceId': 'i-terminated',
+                    'State': {'Name': 'terminated'},
+                },
+            ]}]
+        }]
+        boto3_client = Mock(return_value=regional_client)
+        boto3 = ModuleType('boto3')
+        boto3.client = boto3_client
+        provider = AWSProvider({
+            'access_key_id': 'mock-access',
+            'secret_access_key': 'mock-secret',
+        }, regions=['eu-central-1'])
+
+        with patch.dict(sys.modules, {'boto3': boto3}):
+            instances = provider.list_instances()
+
+        boto3_client.assert_called_once_with(
+            'ec2',
+            aws_access_key_id='mock-access',
+            aws_secret_access_key='mock-secret',
+            region_name='eu-central-1',
+            endpoint_url=None,
+        )
+        regional_client.get_paginator.assert_called_once_with('describe_instances')
+        self.assertEqual([item.instance_id for item in instances], ['i-linux', 'i-windows'])
+        self.assertEqual(instances[0].name, 'api-01')
+        self.assertEqual(instances[0].os_type, 'linux')
+        self.assertEqual(instances[1].name, 'i-windows')
+        self.assertEqual(instances[1].os_type, 'windows')
+
+    @override_settings(CLOUD_SYNC_ALLOWED_ENDPOINTS=())
+    def test_aws_rejects_custom_endpoint_before_sdk_call(self):
+        boto3_client = Mock()
+        boto3 = ModuleType('boto3')
+        boto3.client = boto3_client
+        provider = AWSProvider({'endpoint_url': 'https://untrusted.example.test'}, ['eu-west-1'])
+
+        with patch.dict(sys.modules, {'boto3': boto3}), self.assertRaises(ValidationError):
+            provider.list_instances()
+
+        boto3_client.assert_not_called()
+
+    def test_aws_region_discovery_is_mocked(self):
+        discovery_client = Mock()
+        discovery_client.describe_regions.return_value = {
+            'Regions': [{'RegionName': 'eu-west-1'}, {'RegionName': 'eu-west-3'}]
+        }
+        regional_client = Mock()
+        regional_client.get_paginator.return_value.paginate.return_value = []
+        boto3_client = Mock(side_effect=[discovery_client, regional_client, regional_client])
+        boto3 = ModuleType('boto3')
+        boto3.client = boto3_client
+
+        with patch.dict(sys.modules, {'boto3': boto3}):
+            instances = AWSProvider({}, regions=[]).list_instances()
+
+        self.assertEqual(instances, [])
+        self.assertEqual(
+            [call.kwargs['region_name'] for call in boto3_client.call_args_list],
+            ['us-east-1', 'eu-west-1', 'eu-west-3'],
+        )
+
+    def test_azure_sdk_is_mocked_and_network_addresses_are_normalized(self):
+        credential = object()
+        credential_factory = Mock(return_value=credential)
+        compute = Mock()
+        network = Mock()
+        compute_factory = Mock(return_value=compute)
+        network_factory = Mock(return_value=network)
+        vm = SimpleNamespace(
+            vm_id='vm-guid', id='/subscriptions/sub/resourceGroups/rg/providers/vm/vm-a',
+            name='vm-a', location='westeurope', os_profile=object(),
+            storage_profile=SimpleNamespace(os_disk=SimpleNamespace(os_type='Windows')),
+            hardware_profile=SimpleNamespace(vm_size='Standard_B2s'),
+            network_profile=SimpleNamespace(network_interfaces=[SimpleNamespace(
+                id='/subscriptions/sub/resourceGroups/rg-net/providers/Microsoft.Network/networkInterfaces/nic-a'
+            )]),
+        )
+        compute.virtual_machines.list_all.return_value = [vm]
+        public_ip_ref = SimpleNamespace(
+            id='/subscriptions/sub/resourceGroups/rg-net/providers/Microsoft.Network/publicIPAddresses/pip-a'
+        )
+        network.network_interfaces.get.return_value = SimpleNamespace(ip_configurations=[
+            SimpleNamespace(private_ip_address='10.1.0.4', public_ip_address=public_ip_ref)
+        ])
+        network.public_ip_addresses.get.return_value = SimpleNamespace(ip_address='203.0.113.40')
+        modules = self._azure_modules(credential_factory, compute_factory, network_factory)
+        provider = AzureProvider({
+            'tenant_id': 'tenant-guid', 'client_id': 'client-guid',
+            'client_secret': 'mock-secret', 'subscription_id': 'subscription-guid',
+        }, regions=['westeurope'])
+
+        with patch.dict(sys.modules, modules):
+            instances = provider.list_instances()
+
+        credential_factory.assert_called_once_with(
+            tenant_id='tenant-guid', client_id='client-guid', client_secret='mock-secret'
+        )
+        compute_factory.assert_called_once_with(credential, 'subscription-guid')
+        network_factory.assert_called_once_with(credential, 'subscription-guid')
+        network.network_interfaces.get.assert_called_once_with('rg-net', 'nic-a')
+        network.public_ip_addresses.get.assert_called_once_with('rg-net', 'pip-a')
+        self.assertEqual(len(instances), 1)
+        self.assertEqual(instances[0].instance_id, 'vm-guid')
+        self.assertEqual(instances[0].private_ip, '10.1.0.4')
+        self.assertEqual(instances[0].public_ip, '203.0.113.40')
+        self.assertEqual(instances[0].os_type, 'windows')
+
+    def test_azure_region_filter_avoids_network_lookup(self):
+        compute = Mock()
+        network = Mock()
+        compute.virtual_machines.list_all.return_value = [SimpleNamespace(
+            vm_id='vm-guid', id='vm-id', name='vm-a', location='eastus',
+            os_profile=None, storage_profile=None, hardware_profile=None,
+            network_profile=SimpleNamespace(network_interfaces=[]),
+        )]
+        modules = self._azure_modules(Mock(), Mock(return_value=compute), Mock(return_value=network))
+
+        with patch.dict(sys.modules, modules):
+            instances = AzureProvider({}, regions=['westeurope']).list_instances()
+
+        self.assertEqual(instances, [])
+        network.network_interfaces.get.assert_not_called()
+
+    @staticmethod
+    def _azure_modules(credential_factory, compute_factory, network_factory):
+        azure = ModuleType('azure')
+        identity = ModuleType('azure.identity')
+        identity.ClientSecretCredential = credential_factory
+        mgmt = ModuleType('azure.mgmt')
+        compute = ModuleType('azure.mgmt.compute')
+        compute.ComputeManagementClient = compute_factory
+        network = ModuleType('azure.mgmt.network')
+        network.NetworkManagementClient = network_factory
+        azure.identity = identity
+        azure.mgmt = mgmt
+        mgmt.compute = compute
+        mgmt.network = network
+        return {
+            'azure': azure,
+            'azure.identity': identity,
+            'azure.mgmt': mgmt,
+            'azure.mgmt.compute': compute,
+            'azure.mgmt.network': network,
+        }
 
 
 class CloudSyncQueueTests(SimpleTestCase):
